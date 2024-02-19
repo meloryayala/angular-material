4bb4b61123474fd68b7a4ac358575248b475e0b5
Melory Ayala <melory.ayala@gmail.com>
2024-02-19 16:26:29 +0000

    refactor to pluck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material/sidenav';
+import { pluck } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +9,35 @@ import { MatSidenav } from '@angular/material/sidenav';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isSmallScreen = false;
 
-  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  // ngAfterContentInit(): void {
+//   this.breakpointObserver.observe(['(max-width: 800px)']).subscribe({
+//     next: res => {
+//       if (res.matches) {
+//         this.isSmallScreen = true;
+//       } else {
+//         this.isSmallScreen = false;
+//       }
+//     }
+//   })
+// }
 
   ngAfterContentInit(): void {
-    this.breakpointObserver.observe(['(max-width: 800px)']).subscribe({
-      next: res => {
-        if (res.matches) {
-          this.sidenav.mode = 'over';
-          this.sidenav.close();
-        } else {
-          this.sidenav.mode = 'side';
-          this.sidenav.open();
-        }
-      }
-    })
+    this.breakpointObserver.observe(['(max-width: 800px)'])
+      .pipe(
+        pluck('matches')
+      )
+      .subscribe(res => this.isSmallScreen = res);
+  }
+
+
+
+  get sidenavMode() {
+    return this.isSmallScreen ? 'over' : 'side';
   }
 }
