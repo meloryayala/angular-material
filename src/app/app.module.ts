e9f70f12c2585429bd2e0a3c0dd9c54c9adde50f
Melory Ayala <melory.ayala@gmail.com>
2024-02-20 16:42:27 +0000

    add form field and global configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ButtonIconsComponent } from './button-icons/button-icons.component';
+import { FormFieldsComponent } from './form-fields/form-fields.component';
+import { MatInputModule } from '@angular/material/input';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonIconsComponent,
+    FormFieldsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule,
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatSliderModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatInputModule
 
-  ],
-  providers: [],
+    ],
+  providers: [{
+    provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+    useValue: { appearance: 'outline', floatLabel: 'always'}
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
